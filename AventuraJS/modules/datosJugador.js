52ce37b080ff11f016c29469d6b92e59b22df766
HelMoonflare <alexanderplanton@gmail.com>
2025-12-02 20:50:22 +0100

    Cambios en el mercado y el CSS

--- AventuraJS/modules/datosJugador.js
+++ AventuraJS/modules/datosJugador.js
@@ -3,7 +3,7 @@ import { Jugador } from "../constants/Jugador.js";
 // Creamos la instancia del jugador
 export const jugador = new Jugador(
     "Talion",
-    "images/image.png",
+    "images/talion.png",
     10,     // ataque base
     5,      // defensa base
     100,     // vida base
